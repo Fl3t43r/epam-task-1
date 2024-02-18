87900e743f92bbe77bb475b7653d22fd1d7b672c
xlspring <tsyomav@gmail.com>
2024-02-18 09:26:35 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log('String + boolean:', '5' + true); 
+console.log('String + number:', '5' + 10);    
+console.log('Number + boolean:', 10 + true);  
+
+console.log('String * boolean:', '5' * true); 
+console.log('String * number:', '5' * 10);    
+console.log('Number * boolean:', 10 * true);  
+
+console.log('String / boolean:', '10' / true); 
+console.log('String / number:', '10' / 2);    
+console.log('Number / boolean:', 10 / true);  
+
+console.log('String to number:', Number('10'));     
+console.log('String to boolean:', Boolean('true')); 
+console.log('Number to string:', String(5));        
